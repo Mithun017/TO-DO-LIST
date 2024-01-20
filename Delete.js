67f6ab257a2e95b305ab2fb556580e862b72f766
Mithun017 <153537192+Mithun017@users.noreply.github.com>
2024-01-20 21:33:40 +0530

    Delete Delete.js

--- Delete.js
+++
@@ -1,8 +0,0 @@
-taskList.addEventListener('click', function(event) {
-    
-    if (event.target.classList.contains('delete-button')) {
-        
-        const listItem = event.target.parentNode;
-        taskList.removeChild(listItem);
-    }
-});
